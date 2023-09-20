f54557c4b7acfccda61e989ee213d50842143cca
MavrinSergey <mavrin_sergei@list.ru>
2023-09-20 19:27:37 +0400

    hw6

---
+++ 03_Basics JavaScript/lesson_2 seminar_1/script.js
@@ -0,0 +1,50 @@
+// // Задание 1 (тайминг 20 минут)
+// console.log("Текст из script.js!");
+
+// // Задание 2 (тайминг 25 минут)
+// let num = 123;
+// console.log(num);
+// let a = 5;
+// a = 7;
+// console.log(a);
+// сonst b = 1 + 2 + 3;
+// console.log(b); // вывод переменной на экран
+// /* Создайте 2 вида комментариев в вашем коде,
+// добавьте в них произвольный текст */
+
+// // Задание 3 (тайминг 20 минут)
+// let c = 10;
+// let d = 2;
+// let sum = c + d;
+// let diff = c - d;
+// let div = c / d;
+// console.log(`c - ${c}
+// d - ${d}
+// sum - ${sum}
+// diff - ${diff}
+// div - ${div}`);
+
+// Задание 4 (тайминг 10 минут)
+// 1. 23
+// 2. 6
+// 3. 36;
+// 4. 15;
+// 5. 4;
+// 6. 4;
+
+// // Задание 5 (тайминг 5 минут)
+// let a = 1.5;
+// let b = 0.75;
+// console.log(a + b);
+// console.log(-a);
+
+// // Задание 6 (тайминг 15 минут)
+
+// let name = "Sergey";
+// let lastName = "Mavrin";
+// let java = "java";
+// let script = "script";
+// console.log(java + script);
+// let hello = "hello";
+// let world = "world";
+// console.log(hello + " " + world);
