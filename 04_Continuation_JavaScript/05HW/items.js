21a85884bc3192cd942dd053add97d44622b866c
MavrinSergey <mavrin_sergei@list.ru>
2023-11-29 00:02:06 +0400

    Continuation JS hw05

---
+++ 04_Continuation_JavaScript/05HW/items.js
@@ -0,0 +1,38 @@
+const data = `[
+  {
+    "img": "./img/01.jpg",
+    "head": "Lorem ipsum dolor",
+    "descriptions": "sit amet, consectetur adipisicing elit. Ab aut commodi,",
+    "price": "$52.00"
+  },
+  {
+    "img": "./img/02.png",
+    "head": "cum doloremque eos",
+    "descriptions": "et eum fugiat harum incidunt, maxime necessitatibus nulla odit.",
+    "price": "$46.00"
+  },
+  {
+    "img": "./img/03.png",
+    "head": "perspiciatis porro possimus",
+    "descriptions": "praesentium qui quis repudiandae.",
+    "price": "$94.00"
+  },
+  {
+    "img": "./img/04.png",
+    "head": "ELLERY X M'O CAPSULE",
+    "descriptions": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+    "price": "$45.00"
+  },
+  {
+    "img": "./img/05.png",
+    "head": "ELLERY",
+    "descriptions": "arbiter of cool Kym Ellery teams up with Moda Operandi.",
+    "price": "$33.00"
+  },
+  {
+    "img": "./img/06.png",
+    "head": "ELLERY X M'O",
+    "descriptions": "Known for her sculptural takes on traditional f cool Kym Ellery teams up with Moda Operandi.",
+    "price": "$100.00"
+  }
+]`
