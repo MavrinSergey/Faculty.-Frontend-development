3f66d8b358186263717fa0a9bfb89ec65eb25526
MavrinSergey <mavrin_sergei@list.ru>
2024-02-17 13:40:12 +0400

    Basics_JS__HW01

--- 03_Basics_JavaScript/hw_4/3.js
+++ 03_Basics_JavaScript/hw_4/3.js
@@ -1,18 +1,15 @@
 "use strict";
-
-// Используя Math.random() необходимо сгенерировать массив, содержащий 5 цифр в
-// диапазоне [0, 9].
+// Используя Math.random() вам необходимо генерировать цифры от 0 до 9, и создать массив состоящий из 5 таких элементов
 const arr = [];
 for (let i = 0; i < 5; i++) {
-    arr.push(Math.floor(Math.random() * 4));
+    arr.push(Math.floor(Math.random() * 10));
 }
 console.log("Сгенерированный массив:", arr);
 
-// После создания массива необходимо вывести в консоль следующие значения:
-// 1. Сумму элементов массива
-console.log(arr.reduce((acc, el) => acc + el, 0));
+// 1. Рассчитать сумму элементов этого массива
+console.log("Сумма элементов массива: ", arr.reduce((acc, el) => acc + el, 0));
 
-// 2. Минимальное значение в массиве
+// 2. Найти минимальное число
 let min = arr[0];
 for (let i = 0; i < arr.length; i++) {
     const el = arr[i];
@@ -20,28 +17,19 @@ for (let i = 0; i < arr.length; i++) {
         min = el;
     }
 }
+console.log("Минимальное число в массиве: ", min);
+// 3. Найти есть ли в этом массиве число 3
 
-// 3. Новый массив, содержащий индексы сгенерированного выше массива, в которых
-// значение равно 3.
-// Пример: Если у нас сгенерировался массив [2, 3, 5, 7, 3], то мы должны вывести
-// в консоль массив [1, 4]. Такой массив получился потому что в сгенерированном
-// массиве тройки лежат под индексами 1 и 4. Если троек в сгенерированном массиве
-// не окажется, значит нужно будет вывести пустой массив.
 const indexes = arr
-    .map((value, index) => ({ value, index }))
+    .map((value, index) => ({value, index}))
     .filter((obj) => obj.value === 3)
     .map((obj) => obj.index);
 
 console.log("Массив индексов с числом 3:", indexes);
-// Через reduce
-const indexesReduce = arr.reduce((acc, num, index) => {
-    console.log(acc);
-    if (num === 3) {
-        console.log(acc);
-        acc.push(index);
-        console.log(acc);
-    }
-    console.log(acc);
-    return acc;
-}, []);
-console.log(indexesReduce);
+// Третье задание через reduce
+// const indexesReduce = arr.reduce((acc, num, index) => {
+//     if (num === 3) {
+//         acc.push(index);
+//     }
+//     return acc;
+// }, []);
