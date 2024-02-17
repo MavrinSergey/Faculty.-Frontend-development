6268377fc2b611bc3ad4a42bfeed8a228634dffa
MavrinSergey <mavrin_sergei@list.ru>
2024-02-17 15:14:00 +0400

    Vue__02HW

--- 06_Node.js basics/02_seminar/index.js
+++
@@ -1,9 +0,0 @@
-// import calculateResultSum from "./calc.js";
-require('colors');
-
-const calculateResultSum = require("./calc.js")
-
-const total = calculateResultSum([12.1, 32.2, 43.1], 0.9);
-
-const text = "Общая стоимость покупок: " + total.toString().green + " рублей".yellow;
-console.log(total > 50 ? text.red : text.green);
--- 06_Node.js basics/03_hw/readingFile.js
+++
@@ -1,7 +0,0 @@
-const fs = require("fs");
-const path = require("path");
-
-const pathToFile = path.join(__dirname, "person.json");
-const person = JSON.parse(fs.readFileSync(pathToFile));
-
-fs.writeFileSync(pathToFile, personJson);
