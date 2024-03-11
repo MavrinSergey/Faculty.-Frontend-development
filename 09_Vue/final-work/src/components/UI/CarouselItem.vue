426704075f7265743e1e37e5ee549e5115aad815
MavrinSergey <mavrin_sergei@list.ru>
2024-03-11 21:18:08 +0400

    добавил страницу ProjectDetails

---
+++ 09_Vue/final-work/src/components/UI/CarouselItem.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="carousel-item">
+<img
+    :src="require(`@/assets/img/${item_data.img}`)"
+    alt="img">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CarouselItem",
+  props: {
+    item_data: {
+      type: Object,
+      default: () => {}
+    }
+  }
+}
+</script>
+
+
+<style scoped lang="scss">
+
+</style>
--- 09_Vue/final-work/src/pages/Page5ProjectDetails.vue
+++ 09_Vue/final-work/src/pages/Page5ProjectDetails.vue
@@ -1,10 +1,8 @@
 <template>
   <section class="project-details">
-    <h1>{{ $route.params.id }}</h1>
-    <img class="banner" src="@/assets/img/page3_Banner.jpeg" alt="banner">
+    <HeaderContent/>
     <div class="project-details__descriptions">
-      <h1>{{ id }}</h1>
-      <h2 class="project-details__title">Minimal Look Bedrooms</h2>
+      <h2 class="project-details__title">Minimal Look Bedrooms {{ id }}</h2>
       <p class="project-details__text">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquamsem vitae turpis dignissim maximus. Aliquam
         sollicitudin tellumassa, vbel maximus purus posuere in. Dojrices gravida dignissim. Praesent at nibh in mi
@@ -17,16 +15,29 @@
         viverra felis. Donec ultrices tellus vitae iaculisvd porta. Proin tincidunt ligula id purus porttitor.
       </p>
     </div>
+    <SwiperSlider :carousel_data="sliderItems"/>
   </section>
 </template>
 
 <script>
 
 
+import HeaderContent from "@/components/HeaderContent.vue";
+import SwiperSlider from "@/components/SwiperSlider.vue";
+
 export default {
   name: "Page5ProjectDetails",
-  components: {},
+  components: {SwiperSlider, HeaderContent},
   props: ['id'],
+  data() {
+    return {
+      sliderItems: [
+        {id: 1, name: 'project_1', img: 'project_1.png'},
+        {id: 2, name: 'project_2', img: 'project_2.png'},
+        {id: 3, name: 'project_3', img: 'project_3.png'},
+      ]
+    }
+  }
 }
 </script>
 
@@ -35,15 +46,35 @@ export default {
 .banner {
   margin-bottom: 200px;
 }
+
 .project-details {
+
   &__descriptions {
+    margin: 0 auto 70px;
+    max-width: 658px;
 
   }
+
   &__title {
-
+    margin-bottom: 11px;
+    color: $colorPrimary2;
+    font-family: dm sans-serif;
+    font-size: 50px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 125%;
+    letter-spacing: 1px;
   }
-  &__text {
 
+  &__text {
+    margin-bottom: 30px;
+    color: $colorTextGrey2;
+    font-family: Jost sans-serif;
+    font-size: 22px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 150%;
+    letter-spacing: 0.22px;
   }
 }
 </style>
